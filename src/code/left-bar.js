e0d901aced7d5ae624fbb702667536dff8d7685e
gabrielentediado <gabrielcardosopassos@gmail.com>
2024-11-01 21:24:55 -0300

    🔧 refactor():

--- src/code/left-bar.js
+++ src/code/left-bar.js
@@ -1,14 +1,26 @@
 function openNav(event) {
-    event.stopPropagation();
-    document.getElementById("mySidebar").style.width = "250px";
-};
+    document.getElementById("mySidebar").style.width = "240px";
+}
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-};
+}
+
+var count = 0
+sidebar = document.getElementById("mySidebar")
+left_bar_button = document.querySelector('.Left-side-bar-button')
 
 document.addEventListener('click', function(event) {
-    if (!document.getElementById("mySidebar").contains(event.target) && !document.querySelector('.Left-side-bar-button').contains(event.target)) {
-        closeNav();
+    if (!sidebar.contains(event.target) && !left_bar_button.contains(event.target)) {
+        closeNav()
     }
-});
+})
+
+document.addEventListener('click', function(event){
+    count++
+    if(count % 2 || count == 0){
+        openNav()
+    }else{
+        closeNav()
+    }
+})
